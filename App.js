395001977e6615fa9d24d43a5ba166dcda4eb8ea
Casey Arnold <casey.arnold@me.com>
2020-12-23 14:17:12 -0500

    my first commit

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import Bird from './components/Bird'
 
-export default function App() {
+export default function App () {
+  const screenWidth = Dimensions.get('screen').width
+  const screenHeight = Dimensions.get('screen').height
+  const birdLeft = screenWidth / 2
+  const [birdBottom, setBirdBottom] = useState(screenHeight / 2)
+  const gravity = 3
+  let fallingTimerId
+
+  // start bird falling
+  useEffect(() => {
+    if (birdBottom > 0) {
+      fallingTimerId = setInterval(() => {
+        setBirdBottom(birdBottom => birdBottom - gravity)
+      }, 30)
+
+      return () => {
+        clearInterval(fallingTimerId)
+      }
+    }
+  }, [birdBottom])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Bird
+        birdBottom={birdBottom}
+        birdLeft={birdLeft}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +40,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
